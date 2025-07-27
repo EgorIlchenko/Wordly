e9e2c098d3744c1d34b70f8c69bcc4db213ab84a
Egor <egor.ilchenko23@list.ru>
2025-07-27 13:07:31 +0800

    create table `User`

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,13 +1,7 @@
 __all__ = (
     "db_helper",
-    "UserSession",
-    "Word",
-    "UserWord",
     "Base",
 )
 
 from .base import Base
 from .db_helper import db_helper
-from .user_session import UserSession
-from .user_word import UserWord
-from .word import Word
