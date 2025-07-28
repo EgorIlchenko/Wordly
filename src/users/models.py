9b3426b22aaa7983d16a44ead9c6a806be098443
Egor <egor.ilchenko23@list.ru>
2025-07-28 09:29:55 +0800

    add is_subscribed field to users

--- src/users/models.py
+++ src/users/models.py
@@ -12,5 +12,6 @@ class User(Base):
     is_active: Mapped[bool] = mapped_column(default=True)
     is_verified: Mapped[bool] = mapped_column(default=False)
     is_google_account: Mapped[bool] = mapped_column(default=False)
+    is_subscribed: Mapped[bool] = mapped_column(default=False)
     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(),onupdate=func.now())
