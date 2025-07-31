405e66b9b0203eb45cb38171b02a871e42dae540
Egor <egor.ilchenko23@list.ru>
2025-07-31 11:40:25 +0800

    add new prefix for auth

---
+++ src/core/settings.py
@@ -0,0 +1,83 @@
+from functools import lru_cache
+from pathlib import Path
+
+from pydantic import BaseModel, PostgresDsn, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from starlette.templating import Jinja2Templates
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    auth: str = "/auth"
+    users: str = "/users"
+    words: str = "/words"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_size: int = 50
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
+class LangchainConfig(BaseModel):
+    llm_api_key: SecretStr
+    translation_prompt: str
+
+
+class RabbitMQConfig(BaseModel):
+    user: str
+    password: str
+
+
+class SMTPConfig(BaseModel):
+    host: str
+    port: int
+    user: str
+    password: str
+    from_: str
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(BASE_DIR / ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+        extra="ignore",
+    )
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
+    llm: LangchainConfig
+    rabbitmq: RabbitMQConfig
+    smtp: SMTPConfig
+
+
+@lru_cache
+def get_settings() -> "Settings":
+    return Settings()
+
+
+templates = Jinja2Templates(directory="templates")
