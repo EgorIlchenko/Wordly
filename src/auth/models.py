beb723c6e55f6126555d6b549485fecc206e6af3
Egor <egor.ilchenko23@list.ru>
2025-07-31 11:38:04 +0800

    add table for email verification code

---
+++ src/auth/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timezone, timedelta
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models import Base
+
+
+class EmailVerificationCode(Base):
+    email: Mapped[str] = mapped_column(index=True)
+    code: Mapped[str] = mapped_column()
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc))
+    expires_at: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(timezone.utc) + timedelta(minutes=5)
+    )
+
+    def is_expired(self) -> bool:
+        return datetime.now(timezone.utc) > self.expires_at
