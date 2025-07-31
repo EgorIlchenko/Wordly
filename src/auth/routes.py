a051c8a052f58d6b8b6913468a4d16c1369c1bba
Egor <egor.ilchenko23@list.ru>
2025-07-31 16:05:07 +0800

    add routes for code verify

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,13 +1,10 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request, Depends, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
-from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.status import HTTP_302_FOUND
 
+from .dependencies import get_registration_service, get_verification_service
 from .schemas import UserCreate
-from core.models import db_helper
 from core.settings import templates
-from users.crud import UserStorageProtocol, get_user_storage
-from .services import UserService
+from .services import RegistrationService, VerificationService
 
 router = APIRouter(
     tags=["Auth"],
@@ -22,7 +19,7 @@ async def get_register_page(request: Request):
 @router.post("/register")
 async def register_user(
     request: Request,
-    user_service: UserService = Depends(UserService),
+    registration_service: RegistrationService = Depends(get_registration_service),
 ):
     form = await request.form()
 
@@ -32,18 +29,47 @@ async def register_user(
         "full_name": form.get("full_name"),
         "is_subscribed": form.get("is_subscribed") == "on",
     }
-    user_data = UserCreate.model_validate(raw_data)
 
     try:
-        await user_service.create_user(user_data=user_data)
-        return RedirectResponse(url="/login", status_code=HTTP_302_FOUND)
-    except Exception as e:
+        user_data = UserCreate.model_validate(raw_data)
+        await registration_service.register_user(user_data=user_data)
+        request.session["email"] = user_data.email
+
+        return RedirectResponse(url="/api/v1/auth/verify-email", status_code=302)
+
+    except HTTPException as e:
         return templates.TemplateResponse(
             "register.html",
-            {
-                "request": request,
-                "error": str(e),
-                "email": form.get("email"),
-                "full_name": form.get("full_name"),
-            },
+            {"request": request, "error": e.detail, **raw_data},
+        )
+
+
+@router.get("/verify-email", response_class=HTMLResponse)
+async def get_verify_page(request: Request):
+    email = request.session.get("email")
+
+    if not email:
+        return RedirectResponse(url="/api/v1/auth/register")
+
+    return templates.TemplateResponse("verify_email.html", {"request": request, "email": email})
+
+
+@router.post("/verify-email")
+async def post_verify_email(
+    request: Request,
+    verification_service: VerificationService = Depends(get_verification_service),
+):
+    form = await request.form()
+    email = form.get("email")
+    code = form.get("code")
+
+    try:
+        await verification_service.verify_email(email=email, code=code)
+        request.session.pop("email", None)
+
+        return RedirectResponse("/api/v1/auth/login", status_code=302)
+
+    except HTTPException as e:
+        return templates.TemplateResponse(
+            "verify_email.html", {"request": request, "email": email, "error": e.detail}
         )
