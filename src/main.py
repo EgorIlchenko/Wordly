abea1a4617c47dcdfca1a89192b9916560b30982
Egor <egor.ilchenko23@list.ru>
2025-08-22 18:29:52 +0800

    add main page route and exception_handler

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Request
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
-from starlette.staticfiles import StaticFiles
 
 from api import router as api_router
+from auth.dependencies import get_current_active_auth_user
+from auth.exceptions import AuthException
 from core.models import db_helper
-from core.settings import get_settings
+from core.settings import get_settings, templates
+from users.models import User
 
 settings = get_settings()
 
@@ -36,3 +40,23 @@ main_app.add_middleware(
 )
 
 main_app.mount("/assets", StaticFiles(directory="templates/assets"), name="assets")
+
+
+@main_app.exception_handler(AuthException)
+async def auth_exception_handler(request: Request, exc: AuthException):
+    next_url = f"?next={request.url.path}"
+    if exc.detail == "Token expired" and not exc.redirect_to_login:
+        return RedirectResponse(url=f"/api/v1/auth/refresh{next_url}")
+
+    return RedirectResponse(url=f"/api/v1/auth/login{next_url}")
+
+
+@main_app.get("/")
+async def get_main_page(
+    request: Request,
+    user: User = Depends(get_current_active_auth_user),
+):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "user": user},
+    )
