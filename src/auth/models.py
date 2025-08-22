af6e19d891cb5b4273e1d85841feec0f3947cd6c
Egor <egor.ilchenko23@list.ru>
2025-08-22 18:35:28 +0800

    add RefreshSession model

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,9 +1,14 @@
 from datetime import datetime, timedelta, timezone
+from typing import TYPE_CHECKING
+from uuid import UUID
 
-from sqlalchemy import DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base
+
+if TYPE_CHECKING:
+    from users.models import User
 
 
 class EmailVerificationCode(Base):
@@ -17,6 +22,27 @@ class EmailVerificationCode(Base):
         DateTime(timezone=True),
         default=lambda: datetime.now(timezone.utc) + timedelta(minutes=5),
     )
+
+    def is_expired(self) -> bool:
+        return datetime.now(timezone.utc) > self.expires_at
+
+
+class RefreshSession(Base):
+    user_id: Mapped[UUID] = mapped_column(
+        ForeignKey(
+            "users.id",
+            ondelete="CASCADE",
+        )
+    )
+    refresh_token: Mapped[str] = mapped_column(nullable=False)
+    verifier_hash: Mapped[str] = mapped_column(nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    is_revoked: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+    user: Mapped["User"] = relationship(back_populates="refresh_sessions")
 
     def is_expired(self) -> bool:
         return datetime.now(timezone.utc) > self.expires_at
