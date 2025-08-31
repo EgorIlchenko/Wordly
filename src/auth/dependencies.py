1099fbfd1f0b52c40d04988dfff02a9aceb4952f
Egor <egor.ilchenko23@list.ru>
2025-08-31 16:26:15 +0800

    add the func for create redirect uri

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -6,7 +6,7 @@ from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 from pydantic import EmailStr
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.config import ACCESS_TOKEN_TYPE, TOKEN_TYPE_FIELD, GOOGLE_AUTH_BASE_URL
+from core.config import ACCESS_TOKEN_TYPE, GOOGLE_AUTH_BASE_URL, TOKEN_TYPE_FIELD
 from core.models import db_helper
 from core.settings import get_settings
 from users.crud import SQLAlchemyUserStorage
@@ -166,17 +166,20 @@ async def get_current_active_auth_user(
     return user
 
 
-def generate_google_oauth_redirect_uri() -> str:
+def generate_google_oauth_redirect_uri(state: str) -> str:
     query_params = {
         "client_id": settings.google_auth.client_id,
         "redirect_uri": "http://localhost:8001/api/v1/auth/google",
         "response_type": "code",
-        "scope": " ".join([
-            "openid",
-            "profile",
-            "email",
-        ]),
+        "scope": " ".join(
+            [
+                "openid",
+                "profile",
+                "email",
+            ]
+        ),
         "access_type": "offline",
+        "state": state,
     }
 
     query_string = urllib.parse.urlencode(query_params, quote_via=urllib.parse.quote)
